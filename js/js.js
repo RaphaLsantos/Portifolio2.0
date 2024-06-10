691db958ab6147f27a5be87e1cd8f5fa37c6ef7f
Raphael da Silva <119632711+RaphaLsantos@users.noreply.github.com>
2024-06-10 15:22:23 -0300

    Atualização
    - Area de certificações pronta!
    - Implementação da área de projetos.
    - Efeitos JS adicionadas (modal, arrows, slide).

--- js/js.js
+++ js/js.js
@@ -16,48 +16,92 @@ typeText();
 /*efeito digitar*/
 
 
-/*efeito arrow certificados*/
-let arrow_container = document.getElementById('arrow_animation');
-let arrow = document.getElementById('arrow_certifiction');
-let txt = document.getElementById('text_certificado')
 
-let originalArrowPosition = arrow.style.left;
-let originalArrowContainerWidth = arrow_container.style.width;
+// efeito arrow certificados
+let arrowContainers = document.querySelectorAll('.arrow_certificado');
+let arrows = document.querySelectorAll('.arrow_certifiction');
+let texts = document.querySelectorAll('.text_certificado');
 
-arrow_container.addEventListener('mouseover', function () {
-    arrow.style.position = 'relative';
-    arrow.style.left = '90%';
+arrowContainers.forEach((arrowContainer, index) => {
+    let arrow = arrows[index];
+    let txt = texts[index];
 
-    this.style.width = '95%';
-    this.style.transition = 'all ease .5s';
+    let originalArrowPosition = arrow.style.left;
+    let originalArrowContainerWidth = arrowContainer.style.width;
 
-    txt.style.display = 'flex'
+    arrowContainer.addEventListener('mouseover', function () {
+        arrow.style.position = 'relative';
+        arrow.style.left = '90%';
+
+        this.style.width = '93%';
+        this.style.transition = 'all ease .5s';
+
+        txt.style.display = 'flex';
+    });
+
+    arrowContainer.addEventListener('mouseout', function () {
+        arrow.style.position = '';
+        arrow.style.left = originalArrowPosition;
+
+        this.style.width = originalArrowContainerWidth;
+        this.style.transition = 'all ease .5s';
+
+        txt.style.display = 'none';
+    });
 });
-
-arrow_container.addEventListener('mouseout', function () {
-    arrow.style.position = '';
-    arrow.style.left = originalArrowPosition;
+// efeito arrow certificados
 
 
-    this.style.width = originalArrowContainerWidth;
-    this.style.transition = 'all ease .5s';
 
-    txt.style.display = 'none'
-});
-/*efeito arrow certificados*/
+//abrir modal certificados
+function abrirModal(pdfUrl) {
+    var modal = document.getElementById("myModal");
+    var iframe = document.getElementById("pdfFrame");
+    iframe.src = pdfUrl;
+    modal.style.display = "block";
+}
+
+function fecharModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+// Fecha o modal se o usuário clicar fora do conteúdo do modal
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//abrir modal certificados
 
 
-/*carousel inicio*/
+
+
+/*carousel inicio certificados*/
 let currentIndex = 0;
 const cards = document.querySelectorAll('.card_carousel');
 const totalCards = cards.length;
 
+function showCard(index) {
+    cards.forEach((card, i) => {
+        card.style.display = (i >= index && i < index + 2) ? 'flex' : 'none';
+    });
+}
+
+function navigateCarousel(step) {
+    currentIndex = (currentIndex + step + totalCards) % totalCards;
+    showCard(currentIndex);
+}
+
 document.getElementById('arrow_left').addEventListener('click', function() {
-    navigateCarousel(-2); // Retrocede dois cartões
+    navigateCarousel(-1); // Retrocede dois cartões
 });
 
 document.getElementById('arrow_right').addEventListener('click', function() {
-    navigateCarousel(2); // Avança dois cartões
+    navigateCarousel(1); // Avança dois cartões
 });
 
-/*carousel final*/
+// Inicialização
+showCard(currentIndex);
+/*carousel inicio certificados*/
